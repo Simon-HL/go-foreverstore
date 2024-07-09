8c96a83865f1276a2d8896a1af8410e623882bf2
Simon-HL <Simon.h.l@hotmail.com>
2024-07-09 20:28:40 +0200

    feat: add basic p2p library

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/simon-hl/go-foreverstore/p2p"
+)
 
 func main() {
-	fmt.Println("hei")
+	tcpOpts := p2p.TCPTransportOpts{
+		ListenAddr:    ":3000",
+		HandshakeFunc: p2p.NOPHandshakeFunc,
+		Decoder:       p2p.DefaultDecoder{},
+	}
+	tr := p2p.NewTCPTransport(tcpOpts)
+
+	if err := tr.ListenAndAccept(); err != nil {
+		log.Fatal(err)
+	}
+
+	select {}
+
 }
